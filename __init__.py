12ce6282075df77b97bc8a6577ebccdf61d46761
redlegoreng <redlegoreng@gmail.com>
2012-12-26 15:18:25 +0000

    Initial pluginmaker base

---
+++ __init__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ LayerCombinations
+                                 A QGIS plugin
+ Store and restore layer visibilities
+                             -------------------
+        begin                : 2012-12-26
+        copyright            : (C) 2012 by Olivier Dalang
+        email                : olivier.dalang@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+def name():
+    return "Layer Combinations"
+
+
+def description():
+    return "Store and restore layer visibilities"
+
+
+def version():
+    return "Version 0.1"
+
+
+def icon():
+    return "icon.png"
+
+
+def qgisMinimumVersion():
+    return "1.0"
+
+def author():
+    return "Olivier Dalang"
+
+def email():
+    return "olivier.dalang@gmail.com"
+
+def classFactory(iface):
+    # load LayerCombinations class from file LayerCombinations
+    from layercombinations import LayerCombinations
+    return LayerCombinations(iface)
---
+++ layercombinations.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ LayerCombinations
+                                 A QGIS plugin
+ Store and restore layer visibilities
+                              -------------------
+        begin                : 2012-12-26
+        copyright            : (C) 2012 by Olivier Dalang
+        email                : olivier.dalang@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+# Import the PyQt and QGIS libraries
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from qgis.core import *
+# Initialize Qt resources from file resources.py
+import resources_rc
+# Import the code for the dialog
+from layercombinationsdialog import LayerCombinationsDialog
+
+
+class LayerCombinations:
+
+    def __init__(self, iface):
+        # Save reference to the QGIS interface
+        self.iface = iface
+        # Create the dialog and keep reference
+        self.dlg = LayerCombinationsDialog()
+        # initialize plugin directory
+        self.plugin_dir = QFileInfo(QgsApplication.qgisUserDbFilePath()).path() + "/python/plugins/layercombinations"
+        # initialize locale
+        localePath = ""
+        locale = QSettings().value("locale/userLocale").toString()[0:2]
+
+        if QFileInfo(self.plugin_dir).exists():
+            localePath = self.plugin_dir + "/i18n/layercombinations_" + locale + ".qm"
+
+        if QFileInfo(localePath).exists():
+            self.translator = QTranslator()
+            self.translator.load(localePath)
+
+            if qVersion() > '4.3.3':
+                QCoreApplication.installTranslator(self.translator)
+
+    def initGui(self):
+        # Create action that will start plugin configuration
+        self.action = QAction(
+            QIcon(":/plugins/layercombinations/icon.png"),
+            u"Layer Combinations", self.iface.mainWindow())
+        # connect the action to the run method
+        QObject.connect(self.action, SIGNAL("triggered()"), self.run)
+
+        # Add toolbar button and menu item
+        self.iface.addToolBarIcon(self.action)
+        self.iface.addPluginToMenu(u"&Layer Combinations", self.action)
+
+    def unload(self):
+        # Remove the plugin menu item and icon
+        self.iface.removePluginMenu(u"&Layer Combinations", self.action)
+        self.iface.removeToolBarIcon(self.action)
+
+    # run method that performs all the real work
+    def run(self):
+        # show the dialog
+        self.dlg.show()
+        # Run the dialog event loop
+        result = self.dlg.exec_()
+        # See if OK was pressed
+        if result == 1:
+            # do something useful (delete the line containing pass and
+            # substitute with your code)
+            pass
